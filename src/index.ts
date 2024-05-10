60ae68a4338a0a8d92af62f5df1c8cf9fd003208
gilewskadominika <gilewskadominika@wp.pl>
2024-05-10 23:08:08 +0200

    docker

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    // res.json({message: "Hello Express"});
-    res.sendFile(path.resolve('pages/index.html'));
+    res.json({message: "Hello Express"});
 })
 
 
