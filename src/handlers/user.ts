96d6420e9f61b0ee95890e6766f7960a7df91acc
gilewskadominika <gilewskadominika@wp.pl>
2024-05-30 19:07:45 +0200

    creating user and token

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,15 @@
 import prisma from '../db'
+import {createJWT, hashPassword} from "../modules/auth";
 
 export const createNewUser = async(req, res) => {
-    const user = await prisma
+    const user = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            password: await hashPassword(req.body.password),
+        }
+    });
+
+    const token = createJWT(user);
+
+    res.status(201).json({token});
 };
