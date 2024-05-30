38a2897208622cc2b7de2a98e26857c509efb3dd
gilewskadominika <gilewskadominika@wp.pl>
2024-05-30 18:34:56 +0200

    creating token

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import router from './router';
+import {protect} from "./modules/auth";
 
 const app = express();
 
@@ -9,11 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use('/api/v1', router);
+app.use('/api/v1', protect, router); //monada, jest pipe, przez ktory przsechodzą funkcję i mogą zostać przerzucone
+
 
 app.get("/", (req, res) => {
     res.json({message: "Hello Express"});
 })
 
 
-export default app;
+export default app ;
